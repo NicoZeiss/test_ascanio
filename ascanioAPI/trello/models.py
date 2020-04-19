e9c5f0fc3de8072a9052c8143d8506d32c076ecd
nicolas <zeissern@gmail.com>
2020-04-19 17:31:51 +0200

    init frontend app

--- ascanioAPI/trello/models.py
+++ ascanioAPI/trello/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length = 50, blank=False, default='')
+
+    def __str__(self):
+        return self.name
+
+
+class Card(models.Model):
+    name = models.CharField(max_length=250, blank=False, default='')
+    content = models.TextField()
+    priority = models.IntegerField()
+
+    def __str__(self):
+        return self.name
